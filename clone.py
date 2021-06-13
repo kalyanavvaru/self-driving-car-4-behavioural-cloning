dd1919c87c29271a94e59e949c42515da5eecad9
Kalyan Avvaru <kalyan.avvaru@gmail.com>
2021-06-13 10:48:14 +0000

    Adding so far working state

--- clone.py
+++ clone.py
@@ -7,9 +7,9 @@ from random import shuffle
 import sklearn
 import math
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size):
     num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
+    while 1: 
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
@@ -30,8 +30,20 @@ def generator(samples, batch_size=32):
                 
                 center_angle = float(batch_sample[3])
                 angles.append(center_angle)
-                angles.append(center_angle)
-                angles.append(center_angle)
+                angles.append(center_angle+0.2)
+                angles.append(center_angle-0.2)
+                
+                # Flipping
+                images.append(cv2.flip(center_image,1))
+                angles.append(center_angle*-1.0)
+                
+                images.append(cv2.flip(left_image,1))
+                angles.append((center_angle*-1.0) + 0.2)
+                
+                images.append(cv2.flip(right_image,1))
+                angles.append((center_angle*-1.0) - 0.2)
+                
+                
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -71,33 +83,21 @@ row, col, ch = 160, 320, 3 # Trimmed image format
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch)))
+model.add(Lambda(lambda x: (x/127.5) - 1, input_shape=(row, col, ch)))
 model.add(Cropping2D(cropping=((50,20), (0,0))))
-model.add(Convolution2D(24,5,5,border_mode='valid'))
-model.add(MaxPooling2D((2,2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(36,5,5,border_mode='valid'))
-model.add(MaxPooling2D((2,2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(48,3,3,border_mode='valid'))
-model.add(MaxPooling2D((2,2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64,3,3,border_mode='valid'))
+model.add(Convolution2D(24,5,5,border_mode='valid', subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5,border_mode='valid', subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,3,3,border_mode='valid', subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3,border_mode='valid', subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3,border_mode='valid', subsample=(2,2), activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-model.add(Dense(1164))
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-model.add(Dense(100))  
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-model.add(Dense(50))
-model.add(Dropout(0.2))
-model.add(Activation('relu'))
-model.add(Dense(10))
-model.add(Dropout(0.2))
-model.add(Activation('linear'))
+model.add(Dense(125, activation="relu"))
+model.add(Dropout(0.3))
+model.add(Dense(50,activation="relu"))  
+model.add(Dropout(0.3))
+model.add(Dense(10,activation="relu"))
+model.add(Dropout(0.3))
 model.add(Dense(1))
 
 # compile and fit the model
@@ -105,7 +105,7 @@ model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, steps_per_epoch=ceil(len(train_data)/batch_size), \
                     validation_data=validation_generator, \
                     validation_steps=ceil(len(validation_data)/batch_size), \
-                    epochs=5, verbose=1)
+                    epochs=20, verbose=1)
 model.save('model.h5')
 exit()
     
